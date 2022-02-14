71cd8628007fbc84d73011789f439abfa1153943
Pankaj Chibhrani <pkpankakumar05@gmail.com>
2022-02-15 00:40:26 +0530

    Seocnf commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+    # def home
+    #     render html: 'Hello World'
+    # end    
+
+    def home
+    end  
+
+    # def about
+    #     render html: 'About Page'
+    # end  
+
+    def about
+       
+    end  
+
+end
